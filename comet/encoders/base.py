3dec9d2b0a7d4ccfe9b927ebda075e8c6cd287c1
G. M. Arafat Rahman <rahmang@galdor17.metacentrum.cz>
2025-03-31 22:23:42 +0200

    functionality of returning the word id is added

--- comet/encoders/base.py
+++ comet/encoders/base.py
@@ -171,6 +171,8 @@ class Encoder(nn.Module, metaclass=abc.ABCMeta):
             sample,
             truncation=True,
             max_length=self.max_positions - 2,
+            return_offsets_mapping=True,  # Required for word_ids
+
         )
         input_ids, offsets, label_ids = [], [], []
         for i in range(len(sample)):
